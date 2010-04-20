8d71b201a9d551801e8eee9639e4e16ae9aa88a0
Takuto Hayashi <takuto.hayashi@gmail.com>
2010-04-21 08:30:50 +0900

    Add VObj

---
+++ src/Omicron/VObj.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Omicron
+{
+    public class VObj : IValue
+    {
+        private IDictionary<string, IValue> mMethodTable;
+        
+        public VObj(IDictionary<string, IValue> methodTable)
+        {
+            mMethodTable = methodTable;
+        }
+        
+        public string Show()
+        {
+            IEnumerable<string> methodNames = mMethodTable.Keys;
+            return string.Format(
+                "${{{0}}}",
+                methodNames.Skip(1).Aggregate(
+                    methodNames.ElementAt(1),
+                    (acc, elem) => string.Format("{0}, {1}", acc, elem)
+                )
+            );
+        }
+    }
+}
