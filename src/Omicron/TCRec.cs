21b2bee24ff17f6d8a9b47675a9bb14d5d0e1c8c
Takuto Hayashi <takuto.hayashi@gmail.com>
2010-05-08 23:31:22 +0900

    Add TERec and TCRec

---
+++ src/Omicron/TCRec.cs
@@ -0,0 +1,43 @@
+namespace Omicron
+{
+    public class TCRec : ITypeConst
+    {
+        private static ITypeConst mInstance;
+        
+        public static ITypeConst Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = new TCRec();
+                }
+                return mInstance;
+            }
+        }
+        
+        private TCRec()
+        {
+        }
+        
+        public IType Eval(Unique unique, IType type)
+        {
+            return this;
+        }
+        
+        public bool Equals(IType type)
+        {
+            return this == type;
+        }
+        
+        public IType Replace(Unique unique, IType type)
+        {
+            return this;
+        }
+        
+        public string Show()
+        {
+            return "Rec";
+        }
+    }
+}
